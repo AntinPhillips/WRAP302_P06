b3dee359f0875ad7dbb850d99186f77e1a94f103
Antin <antin.phillips.za@gmail.com>
2017-09-21 12:50:07 +0200

    Finished prac 1.

---
+++ T02/src/Game.java
@@ -0,0 +1,11 @@
+/**
+ * Created by s215055772 on 2017/09/21.
+ */
+public class Game {
+    String[][] values;
+
+    public Game() {
+        values = new String[3][3];
+
+    }
+}
---
+++ T02/src/Server.java
@@ -0,0 +1,5 @@
+/**
+ * Created by s215055772 on 2017/09/21.
+ */
+public class Server {
+}
